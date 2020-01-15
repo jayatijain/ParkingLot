ed086b2a34eb0d7218388336882ce69a0694908e
Jayati Jain <Jayati.Jain@go-mmt.com>
2020-01-15 17:04:08 +0530

    package changes

---
+++ src/main/java/com/gojek/processor/AbstractProcessor.java
@@ -0,0 +1,46 @@
+/**
+ *
+ */
+package com.gojek.processor;
+
+import com.gojek.constants.CommandInputMap;
+import com.gojek.exception.ParkingException;
+import com.gojek.service.AbstractService;
+
+/**
+ * @author vaibhav
+ *
+ */
+public interface AbstractProcessor {
+    public void setService(AbstractService service);
+
+    public void execute(String action) throws ParkingException;
+
+    public default boolean validate(String inputString) {
+        // Split the input string to validate command and input value
+        boolean valid = true;
+        try {
+            String[] inputs = inputString.split(" ");
+            int params = CommandInputMap.getCommandsParameterMap().get(inputs[0]);
+            switch (inputs.length) {
+                case 1:
+                    if (params != 0) // e.g status -> inputs = 1
+                        valid = false;
+                    break;
+                case 2:
+                    if (params != 1) // create_parking_lot 6 -> inputs = 2
+                        valid = false;
+                    break;
+                case 3:
+                    if (params != 2) // park KA-01-P-333 White -> inputs = 3
+                        valid = false;
+                    break;
+                default:
+                    valid = false;
+            }
+        } catch (Exception e) {
+            valid = false;
+        }
+        return valid;
+    }
+}
